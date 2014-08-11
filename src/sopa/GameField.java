ca71c4f2e9410e20b25985199a67210ae8dc735e
David Schilling <davejs92@gmail.com>
2014-08-11 21:44:48 +0200

    extract end detection to game end service

--- src/sopa/GameField.java
+++ src/sopa/GameField.java
@@ -5,100 +5,23 @@ package sopa;
  */
 public class GameField {
 
-    Tile[][] field;
-    private PathState[][] pathStates;
-    private int[] directionsX = new int[]{0, 1, 0, -1};
-    private int[] directionsY = new int[]{1, 0, -1, 0};
+    private Tile[][] field;
+    private final GameEndService gameEndService;
 
 
     public GameField(Tile[][] field) {
+        gameEndService = new GameEndService(field.length, field[0].length);
         this.field = field;
-        pathStates = new PathState[field.length][field[0].length];
     }
 
     public boolean solvedPuzzle() {
-        initializePathStates();
-        return searchFinish(0, 1);
+        return gameEndService.solvedPuzzle(0, 1, field.length, field[0].length, field);
     }
 
-    private void initializePathStates() {
-        for(int i = 0; i < pathStates.length; i++){
-            for(int j = 0; j < pathStates[0].length; j++) {
-                pathStates[i][j] = PathState.UNDEFINED;
-            }
-        }
-    }
 
-    public boolean searchFinish(int x, int y){
-        for(int direction = 0; direction < 4; direction++){
-            int xNew = x + directionsX[direction];
-            int yNew = y + directionsY[direction];
-            if(possibleTile(x, y ,xNew, yNew, direction)) {
-                extendSolution(xNew, yNew);
-                if(!foundFinish(xNew, yNew)){
-                    if(searchFinish(xNew, yNew)) {
-                        return true;
-                    } else {
-                        markAsImpossible(xNew, yNew);
-                    }
-                } else {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
-    private boolean possibleTile(int x, int y, int xNew, int yNew, int direction) {
-        if(xNew >= 0 && xNew < field[0].length && yNew >= 0 && yNew < field.length) {
-            Tile tileNew = field[xNew][yNew];
-            Tile tile = field[x][y];
-            if(tileNew.getTileType() != TileType.NONE && pathStates[xNew][yNew] == PathState.UNDEFINED){
-                if(direction == 0) {
-                    if (tile.isBottom() && tileNew.isTop()) {
-                        return true;
-                    }
-                } else if(direction == 1){
-                    if(tile.isRight() && tileNew.isLeft()) {
-                        return true;
-                    }
-                } else if (direction == 2) {
-                    if(tile.isTop() && tileNew.isBottom()) {
-                        return true;
-                    }
-                } else {
-                    if(tile.isLeft() && tile.isRight()) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private void extendSolution(int xNew, int yNew) {
-        pathStates[xNew][yNew] = PathState.POSSIBLE;
-    }
-
-    private boolean foundFinish(int xNew, int yNew) {
-        if(field[xNew][yNew].getTileType() == TileType.FINISH) {
-            return true;
-        }
-        return false;
-    }
-
-    private void markAsImpossible(int xNew, int yNew) {
-        pathStates[xNew][yNew] = PathState.IMPOSSIBLE;
-    }
-
-    public void printBacktracking(){
-        System.out.println("Backtracking Result :");
-        for (int i = 0; i < pathStates[0].length; i++){
-            for(int j = 0; j < pathStates.length; j++){
-                System.out.print(pathStates[j][i] + "\t");
-            }
-            System.out.println();
-        }
+    public void printBacktracking() {
+        gameEndService.printBacktracking();
     }
     public void printField(){
         System.out.println("Field:");
--- src/sopa/Main.java
+++ src/sopa/Main.java
@@ -3,13 +3,11 @@ package sopa;
 public class Main {
 
     public static void main(String[] args) {
-       // sopa.GameField gameField = new sopa.GameField(2, 2);
         FieldCreator fieldCreator = new FieldCreator();
         GameField gameField = new GameField(fieldCreator.fromString(new String[]{"nnnnn", "smacn", "nninn", "nnfnn"}));
         gameField.shiftLine(true, 0, -1);
         System.out.println(gameField.solvedPuzzle());
         gameField.printBacktracking();
         gameField.printField();
-
     }
 }
