9f7262ca4ab7cb8040a0663e540f46fca8fbeb47
David Schilling <davejs92@gmail.com>
2014-08-28 16:40:13 +0200

    fix shift line bug

--- app/src/main/java/de/sopa/model/GameFieldService.java
+++ app/src/main/java/de/sopa/model/GameFieldService.java
@@ -1,8 +1,6 @@
 package de.sopa.model;
 
 import de.sopa.Tile;
-import de.sopa.model.GameEndService;
-import de.sopa.model.GameField;
 
 /**
  * David Schilling - davejs92@gmail.com
@@ -20,30 +18,31 @@ public class GameFieldService {
     }
 
 
-
     public void printBacktracking() {
         gameEndService.printBacktracking();
     }
-    public void printField(GameField gameField){
+
+    public void printField(GameField gameField) {
         System.out.println("Field:");
-        for(int i = 0; i < gameField.getField()[0].length; i++){
-            for(int j = 0; j < gameField.getField().length; j++){
+        for (int i = 0; i < gameField.getField()[0].length; i++) {
+            for (int j = 0; j < gameField.getField().length; j++) {
                 System.out.print(gameField.getField()[j][i].getTileType() + "\t");
             }
             System.out.println();
         }
     }
-    public void printFieldWay(GameField gameField){
+
+    public void printFieldWay(GameField gameField) {
         System.out.println("Field:");
-        for(int i = 0; i < gameField.getField()[0].length; i++){
-            for(int k = 0; k <3; k++) {
-                for(int j = 0; j < gameField.getField().length; j++){
+        for (int i = 0; i < gameField.getField()[0].length; i++) {
+            for (int k = 0; k < 3; k++) {
+                for (int j = 0; j < gameField.getField().length; j++) {
                     switch (k) {
                         case 0:
                             System.out.print("\t " + gameField.getField()[j][i].isTop() + "\t\t");
                             break;
                         case 1:
-                            System.out.print(gameField.getField()[j][i].isLeft() + " 0\t  " + gameField.getField()[j][i].isRight() +" ");
+                            System.out.print(gameField.getField()[j][i].isLeft() + " 0\t  " + gameField.getField()[j][i].isRight() + " ");
                             break;
                         case 2:
                             System.out.print("\t " + gameField.getField()[j][i].isBottom() + "\t\t");
@@ -54,7 +53,7 @@ public class GameFieldService {
 
                 }
                 System.out.println();
-               // System.out.print(gameField.getField()[j][i].getTileType() + "\t");
+                // System.out.print(gameField.getField()[j][i].getTileType() + "\t");
             }
             System.out.println();
 
@@ -62,29 +61,33 @@ public class GameFieldService {
     }
 
     public void shiftLine(GameField gameField, boolean horizontal, int row, int steps) {
-        if(horizontal) {
-            Tile line[] = new Tile[gameField.getField().length-2];
-            for(int i = 0; i < gameField.getField().length - 2; i++) {
-                line[i] = gameField.getField()[i+1][row+1];
-                gameField.getField()[i+1][row+1] = null;
-            }
-            for(int i = 0; i < gameField.getField().length-2; i++) {
-                int newPosition = i + steps;
-                newPosition = shiftToPositive(newPosition,gameField.getField().length-2);
-                newPosition = newPosition%(gameField.getField().length-2);
-                gameField.getField()[newPosition+1][row+1] = line[i];
+        if (horizontal) {
+            if (row < gameField.getField().length - 2) {
+                Tile line[] = new Tile[gameField.getField().length - 2];
+                for (int i = 0; i < gameField.getField().length - 2; i++) {
+                    line[i] = gameField.getField()[i + 1][row + 1];
+                    gameField.getField()[i + 1][row + 1] = null;
+                }
+                for (int i = 0; i < gameField.getField().length - 2; i++) {
+                    int newPosition = i + steps;
+                    newPosition = shiftToPositive(newPosition, gameField.getField().length - 2);
+                    newPosition = newPosition % (gameField.getField().length - 2);
+                    gameField.getField()[newPosition + 1][row + 1] = line[i];
+                }
             }
         } else {
-            Tile line[] = new Tile[gameField.getField()[0].length-2];
-            for(int i = 0; i < gameField.getField()[0].length-2; i++) {
-                line[i] = gameField.getField()[row+1][i+1];
-                gameField.getField()[row+1][i+1] = null;
-            }
-            for(int i = 0; i < gameField.getField()[0].length-2; i++) {
-                int newPosition = (i+steps);
-                newPosition = shiftToPositive(newPosition,gameField.getField()[0].length-2);
-                newPosition = newPosition%(gameField.getField()[0].length-2);
-                gameField.getField()[row+1][newPosition+1] = line[i];
+            if (row < gameField.getField()[0].length - 2) {
+                Tile line[] = new Tile[gameField.getField()[0].length - 2];
+                for (int i = 0; i < gameField.getField()[0].length - 2; i++) {
+                    line[i] = gameField.getField()[row + 1][i + 1];
+                    gameField.getField()[row + 1][i + 1] = null;
+                }
+                for (int i = 0; i < gameField.getField()[0].length - 2; i++) {
+                    int newPosition = (i + steps);
+                    newPosition = shiftToPositive(newPosition, gameField.getField()[0].length - 2);
+                    newPosition = newPosition % (gameField.getField()[0].length - 2);
+                    gameField.getField()[row + 1][newPosition + 1] = line[i];
+                }
             }
         }
     }
