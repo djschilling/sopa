54dd5ea29656427bc55561e5c6c415db2e474b61
David Schilling <davejs92@gmail.com>
2014-08-28 22:46:47 +0200

    group tileSprites together

--- app/src/main/java/de/sopa/scene/GameScene.java
+++ app/src/main/java/de/sopa/scene/GameScene.java
@@ -11,6 +11,7 @@ import de.sopa.manager.SceneManager;
 import de.sopa.model.GameService;
 import de.sopa.model.GameServiceImpl;
 import de.sopa.observer.Observer;
+import org.andengine.entity.Entity;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.texture.region.ITextureRegion;
@@ -19,26 +20,30 @@ import org.andengine.opengl.texture.region.TextureRegion;
 /**
  * David Schilling - davejs92@gmail.com
  */
-public class GameScene extends BaseScene implements Observer {
+public class GameScene extends BaseScene implements Observer{
 
     private GameService gameService;
     private GestureDetector gestureDetector;
     private Sprite solvedSprite;
+    private Entity tileGroup;
 
 
     public void addTiles() {
+        tileGroup.detachChildren();
         Tile[][] field = gameService.getGameField().getField();
         int width = field.length;
         int heigth = field[0].length;
         int spacePerTile = MainActivity.CAMERA_WIDTH / width;
+        int tilesSceneStartY = getTileSceneStartY(spacePerTile);
+        tileGroup.setPosition(0, tilesSceneStartY);
         int tilePositionY = 0;
         for (int y = 0; y < heigth; y++) {
             int tilePositionX = 0;
             for (int x = 0; x < width; x++) {
                 if (field[x][y].getShortcut() != 'n') {
-                    TextureRegion pTextureRegion = ResourcesManager.getInstance().regionTileMap.get(field[x][y].getShortcut());
+                    TextureRegion pTextureRegion = resourcesManager.regionTileMap.get(field[x][y].getShortcut());
                     Sprite tileSprite = new Sprite(tilePositionX, tilePositionY, spacePerTile, spacePerTile, pTextureRegion, vbom);
-                    this.attachChild(tileSprite);
+                    tileGroup.attachChild(tileSprite);
                 }
                 tilePositionX += spacePerTile;
             }
@@ -48,12 +53,16 @@ public class GameScene extends BaseScene implements Observer {
 
     }
 
+    protected int getTileSceneStartY(int spacePerTile) {
+        return (MainActivity.CAMERA_HEIGHT - (spacePerTile * gameService.getGameField().getField().length)) / 2;
+    }
+
 
     public void setSolved(boolean solved) {
         if (solved) {
-            solvedSprite = new Sprite(0, 0, 50, 50, ResourcesManager.getInstance().regionTileMap.get('s'), vbom);
+            solvedSprite = new Sprite(0, 0, 50, 50, resourcesManager.regionTileMap.get('s'), vbom);
         } else {
-            solvedSprite = new Sprite(0, 0, 50, 50, ResourcesManager.getInstance().regionTileMap.get('i'), vbom);
+            solvedSprite = new Sprite(0, 0, 50, 50, resourcesManager.regionTileMap.get('i'), vbom);
         }
         attachChild(solvedSprite);
     }
@@ -71,10 +80,13 @@ public class GameScene extends BaseScene implements Observer {
     @Override
     public void createScene() {
         initializeLogic();
-        this.clearChildScene();
         addBackground();
+        Tile[][] field = gameService.getGameField().getField();
+        tileGroup = new Entity();
+        attachChild(tileGroup);
         addTiles();
     }
+
 
     private void addBackground() {
         ITextureRegion gameBackgroundRegion = ResourcesManager.getInstance().gameBackgroundRegion;
@@ -92,7 +104,8 @@ public class GameScene extends BaseScene implements Observer {
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                gestureDetector = new GestureDetector(new SwipeGestureDetector(gameService, widthPerTile, widthPerTile, widthPerTile));
+                gestureDetector = new GestureDetector(new SwipeGestureDetector(gameService, widthPerTile,
+                        getTileSceneStartY(widthPerTile) + widthPerTile, widthPerTile));
                 TouchListener touchListener = new TouchListener(gestureDetector);
                 scene.setOnSceneTouchListener(touchListener);
             }
