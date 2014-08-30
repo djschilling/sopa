6cf5eabec961d9142cae27d96c6b4a090644a654
Raphael Schilling <raphaelschiling@gmail.com>
2014-08-30 11:14:58 +0200

    Added UUID to TileSprite

--- app/src/main/java/de/sopa/scene/TileSprite.java
+++ app/src/main/java/de/sopa/scene/TileSprite.java
@@ -4,6 +4,8 @@ import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
+import java.util.UUID;
+
 /**
  * David Schilling - davejs92@gmail.com
  */
@@ -11,11 +13,13 @@ public class TileSprite extends Sprite {
 
     private float startX;
     private float startY;
+    private UUID uuid;
 
     public TileSprite(final float pX, final float pY, final float pWidth, final float pHeight, final ITextureRegion pTextureRegion, final VertexBufferObjectManager vbo){
         super(pX, pY, pWidth, pHeight, pTextureRegion, vbo);
         this.startX = pX;
         this.startY = pY;
+        uuid = UUID.randomUUID();
     }
 
     public float getStartX() {
@@ -32,5 +36,26 @@ public class TileSprite extends Sprite {
 
     public void setStartY(float startY) {
         this.startY = startY;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TileSprite)){
+            return false;
+        }
+
+        TileSprite that = (TileSprite) o;
+
+        if (uuid != null ? !uuid.equals(that.uuid) : that.uuid != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid != null ? uuid.hashCode() : 0;
     }
 }
