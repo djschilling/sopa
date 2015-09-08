3b7ebdff0f8ce28821dfde48a86cda01e0e70e82
David Schilling <davejs92@gmail.com>
2015-09-08 13:40:55 +0200

    adjust extra time calculation to not become bigger then 35

--- app/src/main/java/de/sopa/model/justplay/JustPlayServiceImpl.java
+++ app/src/main/java/de/sopa/model/justplay/JustPlayServiceImpl.java
@@ -12,12 +12,12 @@ public class JustPlayServiceImpl implements JustPlayService {
     private final LevelCreator levelCreator;
     private int leftTime;
     private int lastScore;
-    private int[] difficultyGameSize =      {5,     5,     5,      5,      5,      6,      6,      6 };
-    private int[] difficultyMoves =         {2,     2,     2,      3,      3,      3,      3,      3 };
-    private int[] difficultyTime =          {5,     5,     7,      8,      8,      10,     8,      8 };
-    private int[] difficultyScore =         {5,     10,    20,     50,     200,    1000,   2000,   2000};
-    private int[] difficultyTubesMin =      {3,     3,     4,      3,      5,      6,      8,      10};
-    private int[] difficultyTubesMax =      {3,     4,     9,      5,      9,      10,     12,     16};
+    private int[] difficultyGameSize =      {5,     5,     5,      5,      5,      6,      6,      6,       6 };
+    private int[] difficultyMoves =         {2,     2,     2,      3,      3,      3,      3,      3,       3 };
+    private int[] difficultyTime =          {5,     5,     7,      8,      8,      10,     8,      6,       6 };
+    private int[] difficultyScore =         {5,     10,    20,     50,     200,    1000,   2000,   4000,    8000};
+    private int[] difficultyTubesMin =      {3,     3,     4,      3,      5,      6,      8,      8,       10};
+    private int[] difficultyTubesMax =      {3,     4,     9,      5,      9,      10,     12,     12,      16};
 
     public JustPlayServiceImpl() {
 
@@ -31,14 +31,33 @@ public class JustPlayServiceImpl implements JustPlayService {
     public JustPlayResult calculateResult(JustPlayLevelResult justPlayLevelResult) {
 
         LevelSetting levelSetting = getCurrentLevelSetting();
+
+        leftTime = calculateLeftTime(justPlayLevelResult.getLeftTime(), levelSetting.getExtraTime());
+        int extraTime = calculateExtraTime(justPlayLevelResult.getLeftTime(), levelSetting.getExtraTime());
+
         int currentScore = calculateNewScore(justPlayLevelResult, levelSetting);
         JustPlayResult justPlayResult = new JustPlayResult(levelCount, justPlayLevelResult.getLeftTime(),
-                levelSetting.getExtraTime(), lastScore, currentScore);
+                extraTime, lastScore, currentScore);
         lastScore = currentScore;
-        leftTime = levelSetting.getExtraTime() + justPlayLevelResult.getLeftTime();
         levelCount++;
 
         return justPlayResult;
+    }
+
+    private int calculateExtraTime(int leftTime, int extraTime) {
+        int calculateLeftTime = calculateLeftTime(leftTime, extraTime);
+        if(calculateLeftTime == 35) {
+            return 35 - leftTime;
+        }
+        return extraTime;
+    }
+
+    private int calculateLeftTime(int leftTime, int extraTime) {
+        int calculatedTime = extraTime + leftTime;
+        if(calculatedTime >= 35) {
+            return 35;
+        }
+        return calculatedTime;
     }
 
 
@@ -58,8 +77,10 @@ public class JustPlayServiceImpl implements JustPlayService {
             return new LevelSetting(difficultyGameSize[5], difficultyMoves[5], difficultyTime[5], difficultyScore[5], difficultyTubesMin[5], difficultyTubesMax[5]);
         } else if (levelCount <= 35) {
             return new LevelSetting(difficultyGameSize[5], difficultyMoves[5], difficultyTime[5], difficultyScore[5], difficultyTubesMin[5], difficultyTubesMax[5]);
-        } else {
+        } else if (levelCount <= 40) {
             return new LevelSetting(difficultyGameSize[6], difficultyMoves[6], difficultyTime[6], difficultyScore[6], difficultyTubesMin[6], difficultyTubesMax[6]);
+        } else {
+            return new LevelSetting(difficultyGameSize[7], difficultyMoves[7], difficultyTime[7], difficultyScore[7], difficultyTubesMin[7], difficultyTubesMax[7]);
         }
     }
 
